14aab163e2096747e99ecb60568840166f45c651
benediktledl <benedikt.ledl@lbs4.salzburg.at>
2023-03-16 12:15:02 +0100

    Finished Threads with menu and time-stopping

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -1,5 +1,6 @@
-class CustomThread extends Thread {
+import java.util.Scanner;
 
+class CustomThread extends Thread {
     private String word;
 
     public CustomThread(String name, String word) {
@@ -9,24 +10,61 @@ class CustomThread extends Thread {
 
     @Override
     public void run() {
-        int i = 0;
-        while (i<10) {
-            System.out.println(getName() + " - " + word + " - " + System.currentTimeMillis());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            i++;
-        }
+        System.out.println(getName() + " - " + word + " - " + System.currentTimeMillis());
+    }
+}
+
+class CustomRunnable implements Runnable {
+    private String name;
+    private String word;
+
+    public CustomRunnable(String name, String word) {
+        this.name = name;
+        this.word = word;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " - " + word + " - " + System.currentTimeMillis());
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        CustomThread thread1 = new CustomThread("Thread 1", "LBS");
-        CustomThread thread2 = new CustomThread("Thread 2", "vier");
-        thread1.start();
-        thread2.start();
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        while(choice != 3) {
+            System.out.println("Menue: (1 fuer Thread-Klasse, 2 fuer Runnable-Interface, 3 fuer Beenden):");
+            choice = scanner.nextInt();
+
+            long startTime = System.currentTimeMillis();
+
+            if (choice == 1) {
+                CustomThread thread1 = new CustomThread("Thread 1", "LBS");
+                CustomThread thread2 = new CustomThread("Thread 2", "vier");
+                thread1.start();
+                thread2.start();
+                thread1.join();
+                thread2.join();
+            } else if (choice == 2) {
+                Thread thread1 = new Thread(new CustomRunnable("Thread 1", "LBS"));
+                Thread thread2 = new Thread(new CustomRunnable("Thread 2", "vier"));
+                thread1.start();
+                thread2.start();
+                thread1.join();
+                thread2.join();
+            } else if (choice == 3){
+                System.out.println("Okay, beenden...");
+                System.exit(0);
+            } else {
+                System.out.println("Ungueltige Auswahl");
+                System.exit(1);
+            }
+
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+            System.out.println("Ausfuehrungszeit: " + elapsedTime + " ms");
+
+        }
     }
 }
